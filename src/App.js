f5877327d56f0f2718c5980698f859da3ad04f82
Nabeel Kausari <nabeel.kausari@analyttica.com>
2019-07-03 15:35:05 +0530

    initial project setup

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-md-12">
+          <h1>ATH Precision</h1>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
