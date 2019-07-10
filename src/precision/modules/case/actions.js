8325c176eb73adef1e1b6c95145661e550edc41c
sj0099 <sagar.j@analyttica.com>
2019-07-10 15:37:02 +0530

    Updated get steps and case operations

--- src/precision/modules/case/actions.js
+++ src/precision/modules/case/actions.js
@@ -1,22 +1,64 @@
+import flatten from 'lodash/flatten'
+
 import {fetchLinkAs} from "../../api/helpers";
 import * as types from './types'
 
-export const getCase = () => (dispatch) => {
-  const url = {
-    "method": "GET",
-    "href": "/users/3820/marketplace-courses/1180/solves/1530",
-    "accept": "application/vnd.Analyttica.TreasureHunt.UserSolve+json"
-  };
 
-  dispatch({ type: types.FETCH_CASE_REQUESTED })
-  dispatch({ type: types.FETCH_STEPS_REQUESTED })
+export const addDataSets = (payload) => ({ type: types.ADD_DATASETS, payload });
 
-  fetchLinkAs(url)
-    .then(payload => {
-      dispatch({ type: types.FETCH_CASE_SUCCEEDED, payload })
-      return fetchLinkAs(payload._links.user_steps)
-    })
-    .then(payload => dispatch({ type: types.FETCH_STEPS_SUCCEEDED, payload }))
-    .catch(payload => dispatch({ type: types.FETCH_CASE_FAILED, payload }))
-}
+export const getCase = () => (dispatch, getState) => {
+    dispatch({ type: types.FETCH_CASE_REQUESTED });
+
+    const url = {
+        "method": "GET",
+        "href": "/users/3820/marketplace-courses/1180/solves/1530",
+        "accept": "application/vnd.Analyttica.TreasureHunt.UserSolve+json"
+    };
+    fetchLinkAs(url)
+        .then(({ data_sets, ...payload }) => {
+            dispatch(addDataSets(data_sets));
+            dispatch({ type: types.FETCH_CASE_SUCCEEDED, payload });
+        })
+        .catch(payload => dispatch({ type: types.FETCH_CASE_FAILED, payload }));
+};
+
+export const getSteps = () => (dispatch, getState) => {
+    dispatch({ type: types.FETCH_STEPS_REQUESTED });
+    const { case: { info: { _links } } } = getState();
+    fetchLinkAs(_links.user_steps)
+        .then(payload => {
+            const step_data_sets = flatten(payload.map(data => data.results.filter(ds => ds._links.data !== undefined)));
+            dispatch(addDataSets(step_data_sets));
+            const { case: { data_sets } } = getState();
+
+            data_sets.reduce((data, data_set, index) => {
+                if (!data_set._links.modifies)
+                    return data_sets;
+                const href = data_set._links.modifies.href;
+                const matching = data_sets
+                    .map((ds, index) => ({ ds, index }))
+                    .filter(({ ds }) => !!ds._links.self.href.match(href))
+                    .shift();
+                if (!matching) {
+                    console.log('Cannot find the data set that was modified ', href);
+                    return data_sets.concat(data_sets);
+                }
+                data_sets[matching.index] = {...data_sets[index]}
+                data_sets.splice(index,1);}, []);
+            dispatch({ type: types.FETCH_STEPS_SUCCEEDED, payload });
+        })
+        .catch(payload => dispatch({ type: types.FETCH_STEPS_FAILED, payload }));
+};
+
+
+
+
+
+
+
+
+
+
+
+
 
--- src/precision/modules/case/reducer.js
+++ src/precision/modules/case/reducer.js
@@ -1,7 +1,8 @@
 import * as types from './types';
 
 const initialState = {
-}
+  data_sets: [],
+};
 
 export default (state = initialState, { type, payload }) => {
   switch (type) {
@@ -9,7 +10,13 @@ export default (state = initialState, { type, payload }) => {
     case types.FETCH_CASE_REQUESTED:
       return { ...state, case_loading: true, fetch_case_error: null, fetch_case_succeeded: null };
     case types.FETCH_CASE_SUCCEEDED:
-      return { ...state, case_loading: false, fetch_case_succeeded: true, info: payload };
+      return {
+        ...state,
+        case_loading: false,
+        fetch_case_succeeded: true,
+        info: payload
+    };
+
     case types.FETCH_CASE_FAILED:
       return { ...state, case_loading: false, fetch_case_succeeded: false, fetch_case_error: payload };
 
@@ -19,6 +26,12 @@ export default (state = initialState, { type, payload }) => {
       return { ...state, steps_loading: false, fetch_steps_succeeded: true, steps: payload };
     case types.FETCH_STEPS_FAILED:
       return { ...state, steps_loading: false, fetch_steps_succeeded: false, fetch_steps_error: payload };
+
+    case types.ADD_DATASETS:
+      return {
+        ...state,
+        data_sets: [...state.data_sets, ...payload],
+      };
 
     default:
       return state;
