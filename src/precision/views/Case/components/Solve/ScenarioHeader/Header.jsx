cbd7ff9042c0d8d2afb2dd12a9bdd59e4b22393a
Nishant Agarwal <nishant.agarwal@analyttica.com>
2019-07-22 11:25:02 +0530

    feat : css changes to header

--- src/precision/views/Case/components/Solve/ScenarioHeader/Header.jsx
+++ src/precision/views/Case/components/Solve/ScenarioHeader/Header.jsx
@@ -8,24 +8,29 @@ class ScenarioHeader extends Component {
     render() {
         return (
             <Fragment>
-                <Navbar bg="scenario-header">
-                    <Navbar.Brand href="/">
-                        <img className="logo" src={Logo} alt="Logo"/>
-                    </Navbar.Brand>
-                    <Nav >
-                        <Nav.Item bsPrefix="scenario-item">
-                            <NavLink exact to="/cases/3/4" className='reference' activeClassName="active">Reference</NavLink>
-                        </Nav.Item>
-                        <Nav.Item bsPrefix="scenario-item">
-                            <NavLink to="/cases/3/5/" className='scenarios' activeClassName="active">Scenario 1</NavLink>
-                        </Nav.Item>
-                    </Nav>
-                    <h4 className="case-title">Case: Lowering Attrition Rate</h4>
+                <div style={{display : 'flex'}}>
+                    <Navbar bg="scenario-header">
+                        <Navbar.Brand href="/">
+                            <img className="logo" src={Logo} alt="Logo"/>
+                        </Navbar.Brand>
+                        <Nav >
+                            <Nav.Item bsPrefix="scenario-item">
+                                <NavLink exact to="/cases/3/4" className='reference' activeClassName="active">Reference</NavLink>
+                            </Nav.Item>
+                            <Nav.Item bsPrefix="scenario-item">
+                                <NavLink to="/cases/3/5/" className='scenarios' activeClassName="active">Scenario 1</NavLink>
+                            </Nav.Item>
+                        </Nav>
+                        <h4 className="case-title">Case: Lowering Attrition Rate</h4>
 
-                    {/*<div>*/}
+                        {/*<div>*/}
                         {/*<img src="" alt="Add Scenario"/>*/}
-                    {/*</div>*/}
-                </Navbar>
+                        {/*</div>*/}
+                    </Navbar>
+                    <div className="user-details">
+                        Nabeel Kausari
+                    </div>
+                </div>
                 {this.props.children}
             </Fragment>
         );
